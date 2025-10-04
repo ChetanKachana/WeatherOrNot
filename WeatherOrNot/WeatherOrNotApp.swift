4b7a052f3ad6a9c832be3707afde7ce08533dea3
Chetan Kachana <chetan.kachana@gmail.com>
2025-10-04 14:30:20 -0700

    Initial Commit

---
+++ WeatherOrNot/WeatherOrNotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherOrNotApp.swift
+//  WeatherOrNot
+//
+//  Created by Chey K on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherOrNotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherOrNotTests/WeatherOrNotTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherOrNotTests.swift
+//  WeatherOrNotTests
+//
+//  Created by Chey K on 10/4/25.
+//
+
+import Testing
+@testable import WeatherOrNot
+
+struct WeatherOrNotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
